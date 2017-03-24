e2c1920748ab3d4bb5b45a627486c2155aadf61f
wangqiuli1012 <418674878@qq.com>
2017-03-24 16:54:57 +0800

    add lr

---
+++ Tensorflow/linear_regiression/linear_regiression.py
@@ -0,0 +1,66 @@
+"""
+WangQL
+"""
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+#interactive mode
+###plt.ion()
+n_observations = 100
+xs = np.linspace(-3, 3, n_observations)
+ys = np.sin(xs) + np.random.uniform(-0.5, 0.5, n_observations)
+###ys = np.sin(xs)
+
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+
+W = tf.Variable(tf.random_normal([1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+# Y = W * X + b
+Y_pred = tf.add(tf.multiply(X, W), b)
+
+"""
+reduce_sum
+
+'x' is [[1, 1, 1]
+        [1, 1, 1]]
+tf.reduce_sum(x) ==> 6
+tf.reduce_sum(x, 0) ==> [2, 2, 2]
+tf.reduce_sum(x, 1) ==> [3, 3]
+tf.reduce_sum(x, 1, keep_dims=True) ==> [[3], [3]]
+tf.reduce_sum(x, [0, 1]) ==> 6
+"""
+# sum (Y_pred - Y)^2 / (n_observations - 1)
+cost = tf.reduce_sum(tf.pow(Y_pred - Y, 2)) / (n_observations - 1)
+
+# %% Use gradient descent to optimize W,b
+# Performs a single step in the negative gradient
+learning_rate = 0.01
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+# %% We create a session to use the graph
+n_epochs = 1000
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    #Fit all training data
+    prev_training_cost = 0.0
+    for epoch_i in range(n_epochs):
+        for(x, y) in zip(xs, ys):
+            sess.run(optimizer, feed_dict = {X: x, Y: y})
+        
+        training_cost = sess.run(cost, feed_dict = {X: x, Y: y})
+        print(training_cost)
+        if np.abs(prev_training_cost - training_cost) < 0.000001:
+                break
+        prev_training_cost = training_cost
+
+    yp = Y_pred.eval(feed_dict={X: xs}, session=sess)
+    plt.plot(xs, yp)
+
+plt.plot(xs, ys, 'ro')
+
+plt.show()
+plt.waitforbuttonpress()
