aece1d8ad3fb70077c0fdca132903ed4d9cc191a
wangqiuli1012 <418674878@qq.com>
2017-06-07 16:29:38 +0800

    add files

---
+++ Tensorflow/word2vec/api2vectest.py
@@ -0,0 +1,66 @@
+#-*- coding: UTF-8 -*-
+
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+import math
+import os
+import random
+import zipfile
+
+import numpy as np
+from six.moves import urllib
+from six.moves import xrange  # pylint: disable=redefined-builtin
+import tensorflow as tf
+
+# Step 1: Download the data.
+url = 'http://mattmahoney.net/dc/'
+
+
+def maybe_download(filename, expected_bytes):
+  """Download a file if not present, and make sure it's the right size."""
+  if os.path.exists(filename):
+    return filename
+
+filename = maybe_download('comadobe.zip', 31344016)
+
+
+# Read the data into a list of strings.
+def read_data(filename):
+  """Extract the first file enclosed in a zip file as a list of words"""
+  with zipfile.ZipFile(filename) as f:
+    data = tf.compat.as_str(f.read(f.namelist()[0])).split()
+  return data
+
+words = read_data(filename)
+print('data', words[0:20])
+print('Data size', len(words))
+
+# Read the data into a list of strings.
+def read_data(filename):
+  """Extract the first file enclosed in a zip file as a list of words"""
+  with zipfile.ZipFile(filename) as f:
+    data = tf.compat.as_str(f.read(f.namelist()[0])).split()
+  return data
+
+words = read_data(filename)
+print('data', words[0:20])
+print('Data size', len(words))
+print('test Data', words[0:3000])
