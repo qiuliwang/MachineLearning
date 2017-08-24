2fcb56a917fa68b1380d0c953a5da907e3a5c8d9
WangQL <418674878@qq.com>
2017-08-24 17:09:39 +0800

    update

--- DeepLearning/Code/InceptionNet.py
+++ DeepLearning/Code/InceptionNet.py
@@ -8,6 +8,8 @@ Inception v3
 import tensorflow as tf 
 slim = tf.contrib.slim
 trunc_normal = lambda stddev: tf.truncated_normal_initializer(0.0, stddev)
+import time
+from datetime import datetime
 
 def inception_v3_arg_scope(weight_decay = 0.00004,
                            stddev = 0.1,
@@ -299,6 +301,29 @@ def inception_v3(inputs,
                 end_points['Predictions'] = prediction_fn(logits, scope = 'Predictions')
 
     return logits, end_points
+
+def time_tensorflow_run(session, target, info_string):
+    num_step_burn_in = 10
+    total_duration = 0.0
+    total_duration_squared = 0.0
+
+    for i in range(num_batches + num_step_burn_in):
+        start_time = time.time()
+        _ = session.run(target)
+        duration = time.time() - start_time
+        if i >= num_step_burn_in:
+            if not i % 10:
+                print('%s: step %d, duration = %.3f' %
+                    (datetime.now(), i - num_step_burn_in, duration))
+            total_duration += duration
+            total_duration_squared += duration * duration
+    mn = total_duration / num_batches
+    vr = total_duration_squared / num_batches - mn * mn
+    sd = math.sqrt(vr)
+
+    print('%s: %s across %d steps, %.3f +/- %.3f sec / batch' %
+        (datetime.now(), info_string, num_batches, mn, sd))
+
 
 batch_size = 32
 height, width = 299, 299
